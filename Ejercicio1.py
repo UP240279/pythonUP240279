17f785008ace3998f9c22cbe5c8a829d87e986d1
UP240279 <up240279@gmail.com>
2025-02-16 20:48:20 -0600

    area triangulo

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -2,4 +2,15 @@
 edad = 18
 
 # Altura
-altura = 1.65
+miAltura = 1.65
+
+# Número complejo
+n = 4 + 3j
+
+# Área Triángulo
+base = int(input("Ingresa la base del triángulo "))
+altura = int(input('Ingresa la altura del triángulo'))
+print(type(base))
+print(type(altura))
+area = 0.5 * base * altura
+print("El área del triángulo es" , area)
