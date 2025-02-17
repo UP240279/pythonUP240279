f78d61ff5a027eebf9136df98d17fcad60613289
UP240279 <up240279@gmail.com>
2025-02-16 20:57:47 -0600

    perimetro triangulo

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -8,9 +8,14 @@ miAltura = 1.65
 n = 4 + 3j
 
 # Área Triángulo
-base = int(input("Ingresa la base del triángulo "))
-altura = int(input('Ingresa la altura del triángulo'))
-print(type(base))
-print(type(altura))
-area = 0.5 * base * altura
+base = (input("Ingresa la base del triángulo "))
+altura = (input('Ingresa la altura del triángulo '))
+area = 0.5 (base * altura)
 print("El área del triángulo es" , area)
+
+# Perímetro del triángulo
+ladoA = input('Ingrese la medida del lado a ')
+ladoB = input('Ingrese la medida del lado b ')
+ladoC = input('Ingrese la medida del lado c ')
+perimetro = ladoA + ladoB + ladoC
+print('El perímetro del triángulo es ' , perimetro)
