07c5267726affce614ad81ac8d09534e6572e451
Vanessa <up240279@gmail.com>
2025-02-26 10:30:58 -0600

    programa 9

---
+++ EjerciciosDia4.py
@@ -0,0 +1,44 @@
+## PROGRAMAS DÍA 4
+
+# Programa 1 Concatena la cadena 'Thirty', 'Days', 'Of' 'Python' en una sola cadena, 'Thirty Days Of Python'.
+primeraPalabra = "Thirty"
+segundaPalabra = "Days"
+terceraPalabra = "Of"
+cuartaPalabra = "Python"
+espacio = " "
+cadena1 = primeraPalabra + espacio + segundaPalabra + espacio + terceraPalabra + espacio + cuartaPalabra
+print(cadena1)
+
+# Programa 2 Concatenar la cadena 'Coding', 'For' , 'All' en una sola cadena, "Coding For All".
+palabra1 = "Coding"
+palabra2 = "For"
+palabra3 = "All"
+cadena2 = palabra1 + espacio + palabra2 + espacio + palabra3
+print(cadena2)
+
+# Programa 3 Declare una variable llamada company y asígnele un valor inicial "Coding For All"
+company = "Coding For All"
+
+# Programa 4 Imprima la variable empresa utilizando print()
+print(company)
+
+# Programa 5 Imprima la longitud de la cadena de la empresa utilizando el método len() y print()
+lenCompany = len(company)
+print(lenCompany)
+
+# Programa 6 Cambie todos los caracteres a letras mayúsculas utilizando el método upper()
+mayCompany = company.upper()
+print(mayCompany)
+
+# Programa 7 Cambie todos los caracteres a letras minúsculas utilizando el método lower()
+minCompany = company.lower()
+print(minCompany)
+
+# Programa 8 Utilice los métodos capitalize(), title(), swapcase() para formatear el valor de la cadena Coding For All
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
+# Programa 9 Recortar (slice) la primera palabra de la cadena Coding For All
+
+
