507a10846d0df616ee326d3e416188137997de41
Vanessa <up240279@gmail.com>
2025-03-05 10:32:38 -0600

    dia 4

--- EjerciciosDia4.py
+++ EjerciciosDia4.py
@@ -47,22 +47,85 @@ print(company.capitalize())
 print(company.title())
 print(company.swapcase())
 
-# Programa 9 Recortar (slice) la primera palabra de la cadena Coding For All
+# Programa 9 Recortar (slice) la primera palabra de la cadena "Coding For All"
 print("Programa 9") 
 primPal = company[0:6]
 print(primPal)
 
-# Programa 10 Compruebe si la cadena Coding For All contiene una palabra Coding utilizando el método index, find u otros
+# Programa 10 Compruebe si la cadena Coding For All contiene una palabra "Coding" utilizando el método index, find u otros
 print("Programa 10")
 print(company.find("Coding"))
 subcadena = "Coding"
 print(company.index(subcadena))
+print("La cadena sí contiene la palabra 'Coding' en el índice 0")
 
 # Programa 11 Reemplace la palabra 'Coding' en la cadena 'Coding For All' por Python
 print("Programa 11")
 print(company.replace("Coding" , "Python"))
 
-# Programa 12 Cambie Python for All a Python for Everyone using the replace method or other methods
+# Programa 12 Cambie Python for All a Python for Everyone utilizando el método de replace u otros métodos
 print("Programa 12")
-print(company.replace("Coding for All" , "Python for Everyone"))
+print(company.replace("Coding For All" , "Python for Everyone"))
+
+# Programa 13 Divida la cadena 'Coding For All' usando el espacio como separador (split())
+print("Programa 13")
+print(company.split( ))
+
+# Programa 14 "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" divide la cadena en la coma
+print("Programa 14")
+programas = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(programas.split(", "))
+
+# Programa 15 ¿Cuál es el caracter en el índice 0 en la cadena "Coding For All"?
+print("Programa 15")
+print("El caracter en el índice 0 de la cadena es" , company[0])
+
+# Programa 16 ¿Cuál es el último índice de la cadena "Coding For All"?
+print("Programa 16")
+print("El último índice de la cadena es" , company[-1])
+
+# Programa 17 ¿Qué caracter está en el índice 10 en la cadena "Coding For All"?
+print("Programa 17")
+print("El caracter en el índice 10 es" , company[10])
+print("(El caracter 10 es un espacio)")
+
+# Programa 18 Crea un acrónimo o una abreviatura para el nombre 'Python For Everyone'
+print("Programa 18")
+nombre = 'Python For Everyone'
+acronimo = nombre.split()
+print(acronimo[0][0] + acronimo[1][0] + acronimo[2][0])
+
+# Programa 19 Crea un acrónimo o una abreviatura para el nombre 'Coding For All'
+print("Programa 19")
+nombre2 = 'Coding For All'
+acronimo2 = nombre2.split()
+print(acronimo2[0][0] + acronimo2[1][0] + acronimo2[2][0])
+
+# Programa 20 Utilice el index para determinar la posición de la primera aparición de C en 'Coding For All'
+print("Programa 20")
+print(company.index("C"))
+
+# Programa 21 Utilice el index para determinar la posición de la primera aparición de F en 'Coding For All'
+print("Programa 21")
+print(company.index("F"))
+
+# Programa 22 Utilice rfind para determinar la posición de la última aparición de l en 'Coding For All People'
+print("Programa 22")
+oracion = "Coding For All People"
+print(oracion.rfind("l"))
+
+# Programa 23 Utilice index o find para encontrar la posición de la primera aparición de la palabra 'because' en la siguiente oración:
+# 'You cannot end a sentence with because because because is a conjunction'
+print("Programa 23")
+sentence = 'You cannot end a sentence with because because because is a conjunction'
+sentenceSplit = sentence.split( )
+print(sentenceSplit.index("because"))
+
+# Programa 24 Utilice rindex para encontrar la posición de la primera aparición de la palabra 'because' en la siguiente oración:
+# 'You cannot end a sentence with because because because is a conjunction'
+print("Programa 24")
+word = "because"
+print(sentenceSplit.rindex(word))
+
+
 
