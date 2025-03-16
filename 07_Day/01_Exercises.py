a8a12bfa4ef6cc34137c706d08f1b742746e50cd
Vanessa <up240279@gmail.com>
2025-03-16 02:09:17 -0600

    Ejercicios 1 y 2

---
+++ 07_Day/01_Exercises.py
@@ -0,0 +1,35 @@
+## PROGRAMAS DÍA 7
+
+# Ejercicios: Nivel 1
+print("Ejercicios: Nivel 1")
+
+# Sets
+itCompanies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+print("Set:" , itCompanies)
+
+# Programa 1 Encuentra la longitud del set itCompanies
+print("Programa 1")
+print("Longitud:" , len(itCompanies))
+
+# Programa 2 Añade 'Twitter' a itCompanies
+print("Programa 2")
+itCompanies.add("Twitter")
+print(itCompanies)
+
+# Programa 3 Insertar varias empresas de TI a la vez en el set itCompanies
+print("Programa 3")
+itCompanies.update(["Intel", "Tesla", "Samsung", "Adobe"])
+print(itCompanies)
+
+#Programa 4 Eliminar una de las empresas del set itCompanies
+print("Programa 4")
+itCompanies.remove("Apple")
+print(itCompanies)
+
+# Programa 5 ¿Cuál es la diferencia entre eliminar y descartar? (remove y discard)
+print("Programa 5")
+itCompanies.remove("Intel")
+print("Si el elemento no existe en el set, el comando remove generará error")
+itCompanies.discard("Oracle")
+print("Si el elemento no existe en el set, el comando discard no gererará ningún error")
+print(itCompanies)
---
+++ 07_Day/02_Exercises.py
@@ -0,0 +1,47 @@
+## Ejercicios: Nivel 2
+print("Ejercicios: Nivel 2")
+
+# Sets
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+print("Set 1:" , A)
+print("Set 2:" , B)
+
+# Programa 1 Unir A y B
+print("Programa 1")
+union = A.union(B)
+print(union)
+
+# Programa 2 Encuentra la intersección de A y B
+print("Programa 2")
+intersection = A.intersection(B)
+print(intersection)
+
+# Programa 3 Es A un subset de B
+print("Programa 3")
+subset = A.issubset(B)
+print(subset)
+
+# Programa 4 ¿Son A y B sets disjuntos?
+print("Programa 4")
+disjoint = A.isdisjoint(B)
+print(disjoint)
+
+# Programa 5 Unir A con B y B con A
+print("Programa 5")
+unionAB = A.union(B)
+print(unionAB)
+unionBA = B.union(A)
+print(unionBA)
+
+# Programa 6 ¿Cuál es la diferencia simétrica entre A y B?
+print("Programa 6")
+symmetricDifference = A.symmetric_difference(B)
+print(symmetricDifference)
+
+# Programa 7 Eliminar los sets por completo
+print("Programa 7")
+del A
+del B
+print("Los sets ya no existen")
+
