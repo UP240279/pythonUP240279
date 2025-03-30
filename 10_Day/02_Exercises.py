ce2af24a30019128a3db7c4e5692d2fc4e199679
Vanessa <up240279@gmail.com>
2025-03-30 00:30:39 -0600

    avance dia 10-2

---
+++ 10_Day/02_Exercises.py
@@ -0,0 +1,29 @@
+# Ejercicios: Nivel 2
+print("Ejercicios: Nivel 2")
+
+
+# Programa 1 
+# Use for loop to iterate from 0 to 100 and print the sum of all numbers
+print("Programa 1")
+num = 0
+sum = 0
+while num <= 100:
+    sum = sum + num
+    num += 1
+print("La suma de todos los números es:" , sum)
+
+
+
+# Programa 2 
+# Use for loop to iterate from 0 to 100
+# Print the sum of all evens and the sum of all odds
+print("Programa 2")
+evens = 0
+odds = 0
+for n in range(101):
+    if n%2==0:
+        evens = evens + n
+    else:
+        odds = odds + n
+print("La suma de todos los pares es de:" , evens)
+print("La suma de todos los impares es de:" , odds)
