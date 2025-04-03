9b776e0c336fceedce60c50e651e928844330aa1
Vanessa <up240279@gmail.com>
2025-04-03 11:49:29 -0600

    avance dia 15

---
+++ 15_Day/01_Exercises.py
@@ -0,0 +1,51 @@
+# PROGRAMAS DÍA 15
+print("PROGRAMAS DÍA 15")
+print(" ")
+
+
+# Programa 1
+# Open you python interactive shell and try all the examples covered in this section.
+print ("Programa 1: Ejemplos")
+
+# Ejemplo 1
+# SyntaxError
+print("Ejemplo 1: SyntaxError")
+# print"Hola mundo"    # Error: Faltan paréntesis para que se pueda imprimir.
+print('Hola mundo')    
+
+
+# Ejemplo 2
+# NameError
+print("Ejemplo 2: NameError")
+# print(age)     # Error: No está definida la variable.
+age = 18
+print(age)
+
+
+# Ejemplo 3
+# IndexError
+print("Ejemplo 3: IndexError")
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(numbers[10])     # Error: El índice está fuera de rango.
+print(numbers[9])
+
+
+# Ejemplo 4
+# ModuleNotFoundError
+print("Ejemplo 4: ModuleNotFoundError")
+# import maths     # Error: No hay módulo llamado 'maths'.
+import math
+
+# Ejemplo 5
+# AttributeError
+print("Ejemplo 5: AttributeError")
+# pi = math.PI     # Error: 'math' no tiene atributo 'PI'.
+pi = math.pi
+print(pi)
+
+# Ejemplo 6
+# KeyError
+print("Ejemplo 6: KeyError")
+
+
+
