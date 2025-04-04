bd54093990a3b142b529fcd97cb026885981041e
Vanessa <up240279@gmail.com>
2025-04-04 00:37:48 -0600

    final dia 15

--- 15_Day/01_Exercises.py
+++ 15_Day/01_Exercises.py
@@ -10,7 +10,7 @@ print ("Programa 1: Ejemplos")
 # Ejemplo 1
 # SyntaxError
 print("Ejemplo 1: SyntaxError")
-# print"Hola mundo"    # Error: Faltan paréntesis para que se pueda imprimir.
+# print"Hola mundo"    # Error: Faltan los paréntesis para que se pueda imprimir.
 print('Hola mundo')    
 
 
@@ -36,6 +36,7 @@ print("Ejemplo 4: ModuleNotFoundError")
 # import maths     # Error: No hay módulo llamado 'maths'.
 import math
 
+
 # Ejemplo 5
 # AttributeError
 print("Ejemplo 5: AttributeError")
@@ -43,9 +44,39 @@ print("Ejemplo 5: AttributeError")
 pi = math.pi
 print(pi)
 
+
 # Ejemplo 6
 # KeyError
 print("Ejemplo 6: KeyError")
+user = {'name':'Vanessa', 'age':18, 'country':'Mexico'}
+# print(user['county'])     # Error: La clave es incorrecta (no está bien escrito).
+print(user["country"])
 
 
+# Ejemplo 7
+# TypeError
+print("Ejemplo 7: TypeError")
+# print(5 + "7")     # Error: No se puede sumar un entero y una cadena.
+print(5 + int("7"))
 
+
+# Ejemplo 8
+# ImportError
+print("Ejemplo 8: ImportError")
+# from math import power     # Error: No existe "power" en "math".
+from math import pow
+print(pow(4, 5))
+
+
+# Ejemplo 9
+# ValueError
+print("Ejemplo 9: ValueError")
+# print(int("34c"))     # Error: "34c" no es un número válido porque tiene la letra "c".
+print(int("34"))
+
+
+# Ejemplo 10
+# ZeroDivisionError
+print("Ejemplo 10: ZeroDivisionError")
+# print(2 / 0)     # Error: No se puede dividir por 0.
+print(2 / 1)
